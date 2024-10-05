340a6f3e0cdd5d097915797bcfa11b06efe491ce
Prasadprd <prasadchaudhari117@gmail.com>
2024-10-05 16:57:05 +0530

    Initial commit

---
+++ src/models/index.js
@@ -0,0 +1,4 @@
+module.exports.Token = require('./token.model');
+module.exports.User = require('./user.model');
+module.exports.Genre = require('./genere.model');
+module.exports.Content = require('./content.model');
